126b7900332669089ba11029dae6e4b36e9c832d
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-07-03 01:48:58 +0530

    golden ratio formula for fibonacchi que

--- src/Recursion/fiboNacii.java
+++ src/Recursion/fiboNacii.java
@@ -2,9 +2,16 @@ package Recursion;
 
 public class fiboNacii {
     public static void main(String[] args) {
-        int ans = fibo(4);
-        System.out.println(ans);
+//        int ans = fibo(4);
+//        System.out.println(ans);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboformula(i));
+        }
 
+    }
+
+    static int fiboformula(int n ){
+        return (int)((Math.pow(((1+Math.sqrt(5)) /2) ,n) - Math.pow(((1-Math.sqrt(5))/2),n)) / Math.sqrt(5));
     }
 
     static int fibo(int n){
