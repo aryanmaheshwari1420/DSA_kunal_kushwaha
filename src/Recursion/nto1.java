c5b1240b5ba65b5a71ce4368437fb895c328be94
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-07-04 13:10:14 +0530

    two more file in recursion

---
+++ src/Recursion/nto1.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class nto1 {
+    public static void main(String[] args) {
+        Nto1(5);
+        reverse(5);
+    }
+
+    static void Nto1(int n){
+        if (n==0){
+            return;
+        }
+        else
+            System.out.println(n);
+        Nto1(n-1);
+    }
+
+    static void reverse(int n){
+        if (n==0){
+            return;
+        }
+        else
+            reverse(n-1);
+        System.out.println(n);
+
+    }
+}
