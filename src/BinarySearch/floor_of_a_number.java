94e19ab73ab6449c5116496a2fe2115dd858c9c6
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-08-12 01:18:12 +0530

    adding file in binarysearch

--- src/BinarySearch/floor_of_a_number.java
+++ src/BinarySearch/floor_of_a_number.java
@@ -1,16 +1,18 @@
 package BinarySearch;
 
- // ceiling of a number - --> Smallest element in the array greater or = target
 
-public class Ceiling_of_a_number {
+ //  floor = greater number smallest or  = target
 
-
-
+public class floor_of_a_number {
     public static void main(String[] args) {
 
     }
 
-    static  int floor(int arr[] , int target){
+    static  int CeilingNumber(int arr[] , int target){
+        // but what if the target is greater than the greatest number in the array
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
         int start = 0;
         int end = arr.length -1;
         while (start<=end){
@@ -25,6 +27,9 @@ public class Ceiling_of_a_number {
                 start = mid +1;
             }
         }
-        return end;
+        return start;
     }
+
+
+
 }
---
+++ src/BinarySearch/largest_greatestletter.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class largest_greatestletter {
+    public static void main(String[] args) {
+
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
+
