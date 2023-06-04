b7ae93e23695112a05c3e3243c6b42bc32e88cef
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-06-04 23:28:20 +0530

    New package added --> Tree and 2 fils added

--- src/Tree/BST.java
+++ src/Tree/BST.java
@@ -1,68 +1,102 @@
-package Tree;
+class BST {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
 
-public class BST {
-    private Node root;
-    class Node{
-        int value;
-        int height;
-        Node left;
-        Node right;
-
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
         }
-        public int getValue(){
+
+        public int getValue() {
             return value;
         }
     }
-    public int height(Node node){
-        if(node==null){
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public int height(Node node) {
+        if (node == null) {
             return -1;
         }
         return node.height;
     }
-    public boolean isEmpty(){
-        return root==null;
-    }
-    public void display(){
-        display(root,"Root Node: ");
-    }
-    private void display(Node node,String details){
-        if(node==null){
-            return;
-        }
-        System.out.println(details + node.getValue());
-        display(node.left, " Left  Child of " + node.getValue() + " : ");
-        display(node.right," Right Child of " + node.getValue() + " : ");
 
+    public boolean isEmpty() {
+        return root == null;
     }
-    private Node insert(int value,Node node){
 
-        if(node==null){
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
             node = new Node(value);
             return node;
         }
-        if(value<node.value){
-            node.left = insert(value,node.left);
-        }
-        if(value>node.value){
-            node.right = insert(value,node.right);
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
         }
 
-        node.height  = Math.max(height(node.left),height(node.right)) +1;
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
 
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
         return node;
     }
 
-    public boolean balanced(){
-        return balanced(root);
-    }
-    public boolean balanced(Node node){
-        if(node==null){
-            return true;
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
         }
-        return Math.abs(height(root.left) - height(root.right)) <=1 && balanced(node.left)  && balanced(node.right);
     }
 
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
 
 }
